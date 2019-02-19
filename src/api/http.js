628f00d925a22383c9f87f91117bdbf453b6cc15
yinshuaibin <805346635@qq.com>
2019-02-19 09:48:25 +0800

    修改项目不能运行的bug

---
+++ src/api/http.js
@@ -0,0 +1,116 @@
+// http.js
+// ajax工具
+import axios from 'axios'
+// npm仓库管理的包,可通过npm install qs进行安装
+import qs from 'qs'
+// 引入请求的地址
+import baseUrl from '@/utils/globalConfig.js'
+// 引入消息提示
+import Message from 'iview/src/components/message/index'
+
+import router from '@/router/index.js'
+
+// 设置提示
+Message.config({
+  top: 350,
+  duration: 6
+})
+// ajax统一请求设置
+var instance = axios.create({
+  baseURL: baseUrl + '/y',
+  // 序列化
+  paramsSerializer: function (params) {
+    return qs.stringify(params, {
+      arrayFormat: 'repeat'
+    })
+  },
+  // 带cookie请求
+  withCredentials: true, // 默认的
+  validateStatus: function (status) {
+    return status >= 200 && status < 400 // 设置默认合法请求状态码
+  },
+  timeout: 100000
+})
+// 响应时拦截
+instance.interceptors.response.use(response => {
+  // 返回响应时做一些处理
+  return response
+}, error => {
+  if (!error.response) {
+    router.push({path: '/login', query: {errorCode: '399'}})
+    return
+  }
+  // 当响应异常时做一些处理
+  let e = error.response.data
+  if (e) {
+    Message.error(e.error)
+  } else {
+    Message.error(error.message + ', 服务端异常说明为空')
+  }
+  return Promise.reject(error)
+})
+// 封装get方法, post方法
+// 返回Promise对象
+/**
+ * get请求
+ * @param { String} url 请求的url地址
+ * @param { Object} obj 请求参数,序列化后追加到url上
+ */
+export function get (url, obj) {
+  return new Promise((resolve, reject) => {
+    instance.get(url, {
+      params: obj
+    }).then(function (response) {
+      resolve(response.data)
+    }).catch(function (err) {
+      reject(err)
+    })
+  })
+}
+
+export function post (url, params) {
+  return new Promise((resolve, reject) => {
+    instance.post(url, params).then(function (response) {
+      resolve(response.data)
+    }).catch(function (err) {
+      reject(err)
+    })
+  })
+}
+
+export function put (url, params) {
+  return new Promise((resolve, reject) => {
+    instance.put(url, params).then(function (response) {
+      resolve(response.data)
+    }).catch(function (err) {
+      reject(err)
+    })
+  })
+}
+
+export function patch (url, params) {
+  return new Promise((resolve, reject) => {
+    instance.patch(url, params).then(function (response) {
+      resolve(response.data)
+    }).catch(function (err) {
+      reject(err)
+    })
+  })
+}
+
+/**
+ * 将参数追加到请求地址上,然后发送删除请求
+ * @param {* String} url 请求地址
+ * @param {* Object} obj 请求对象,delete操作,请求参数会追加到url后面
+ */
+export function remove (url, obj) {
+  return new Promise((resolve, reject) => {
+    instance.delete(url, {
+      params: obj
+    }).then(function (response) {
+      resolve(response.data)
+    }).catch(function (err) {
+      reject(err)
+    })
+  })
+}
---
+++ static/img/homeIndex.vue
@@ -0,0 +1,151 @@
+<style scoped>
+.home{
+    height: 700px;
+}
+
+.btn-set {
+  width: 380px;
+  height: 220px;
+  margin: 30px 60px;
+  border: none;
+  color: white;
+  font-size: 2.0rem /* 30/16 */
+}
+</style>
+<template>
+    <div class="home">
+        <Row>
+            <Button v-for="menu,index in menus" :key="index" :style="menu.style" class="btn-set" @click="menuSelectHandler(menu.component)">
+                {{menu.title}}
+            </Button>
+        </Row>
+
+        <div style="position: relative">
+        </div>
+    </div>
+</template>
+<script>
+import Widget from "../base/Widget.vue";
+export default {
+  name: "HomeIndex",
+  data() {
+    return {
+      menus: [
+        {
+          title: "新流入人员",
+          component: "clusterClassification",
+          style: {
+            backgroundColor: "#e18f84"
+          }
+        },
+        {
+          title: "报警查询",
+          component: "warning",
+          style: {
+            backgroundColor: "#6ceeaa"
+          }
+        },
+        {
+          title: "布控统计",
+          component: "alarmStatistic",
+          style: {
+            backgroundColor: "#dcedaa"
+          }
+        },
+        {
+          title: "动态检索",
+          component: "dynamic",
+          style: {
+            backgroundColor: "#d265c2"
+          }
+        },
+        {
+          title: "历史查询",
+          component: "history",
+          style: {
+            backgroundColor: "#fed2c8"
+          }
+        },
+        {
+          title: "实时监控",
+          component: "monitor",
+          style: {
+            backgroundColor: "#bcedaa"
+          }
+        },
+        {
+          title: "档案反查",
+          component: "clusterSearch",
+          style: {
+            backgroundColor: "#347ebf"
+          }
+        },
+        {
+          title: "档案浏览",
+          component: "clusterBrowse",
+          style: {
+            backgroundColor: "#d1c0a4"
+          }
+        },
+        {
+          title: "用户添加",
+          component: "userAdd",
+          style: {
+            backgroundColor: "#e18f84"
+          }
+        },
+        {
+          title: "用户列表",
+          component: "userList",
+          style: {
+            backgroundColor: "#e18f84"
+          }
+        },
+        {
+          title: "角色添加",
+          component: "roleAdd",
+          style: {
+            backgroundColor: "#e18f84"
+          }
+        },
+        {
+          title: "角色列表",
+          component: "roleList",
+          style: {
+            backgroundColor: "#e18f84"
+          }
+        },
+        {
+          title: "摄像头列表",
+          component: "cameraList",
+          style: {
+            backgroundColor: "#e18f84"
+          }
+        },
+        {
+          title: "新增摄像头",
+          component: "addCamera",
+          style: {
+            backgroundColor: "#e18f84"
+          }
+        },
+        {
+          title: "相机分组",
+          component: "cameraSetting",
+          style: {
+            backgroundColor: "#e18f84"
+          }
+        }
+      ]
+    };
+  },
+  components: {},
+  watch: {},
+  computed: {},
+  methods: {
+    menuSelectHandler(name) {
+      this.$store.commit("menuSelectEvent", name);
+    }
+  }
+};
+</script>
